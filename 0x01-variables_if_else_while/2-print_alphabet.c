b7516aa8ec2b7d6f9bb63bb7e2759c62aae348f3
darsh <tefakong33@gmail.com>
2023-03-17 16:14:38 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,16 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	char c;
 
 	c = 'a';
 	while
-		(c <= 'z'){
+		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
 	putchar('\n');
-	return(0);
+	return (0);
 }
