5eff95042895b7b80b897ae87d6b23c41af7694d
darsh <tefakong33@gmail.com>
2023-03-17 18:08:55 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,15 +19,16 @@ int main(void)
 		while (digit2 <= 9)
 		{
 			digit3 = 0;
-			while(digit3 <= 9)
+			while (digit3 <= 9)
 			{
-				if(digit1 != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
+				if (digit1 != digit2 && digit1 < digit2 &&
+									digit2 != digit3 && digit2 < digit3)
 				{
 					putchar(digit1 + 48);
 					putchar(digit2 + 48);
 					putchar(digit3 + 48);
 
-					if ( digit1 + digit2 + digit3 != 24)
+					if (digit1 + digit2 + digit3 != 24)
 					{
 						putchar(',');
 						putchar(' ');
