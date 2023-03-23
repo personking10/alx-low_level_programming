f42afef83f2f2cb09fabe53fc00b4fce048bf8e5
darsh <tefakong33@gmail.com>
2023-03-23 06:29:31 -0700

    a

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,11 @@
  *
  * @c: input for alphabet
  *
- * Return: 1 if its uppercase, 0 if not 
+ * Return: 1 if its uppercase, 0 if not
  */
 int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
