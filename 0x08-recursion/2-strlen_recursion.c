d967e3a6f45d5bfeba4e7cb599968e2734d3c56d
darsh <tefakong33@gmail.com>
2023-04-05 14:55:42 -0700

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,16 @@
 /**
  * _strlen_recursion - return length of string
  * @s: pointer to string
- * Return: integer
+ * Return: int
  */
 
 int _strlen_recursion(char *s)
 {
 	int a;
 
-	a = _strlen_recursion(*s);
-	printf("%d\n", a);
+	if (*s > '\0')
+	{
+		a += _strlen_recursion(s + 1) + 1;
+	}
+	return (a);
 }
