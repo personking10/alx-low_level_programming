738df401bb6667b45b17e2a2f8045c5970013a4a
darsh <tefakong33@gmail.com>
2023-04-11 17:13:29 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,7 +39,7 @@ char **strtow(char *str)
 		return (NULL);
 	na = wrad(str);
 	if (na == 1)
-		return (NULL);
+		return (NULL);s
 	wa = (char **)malloc(na * sizeof(char *));
 	if (wa == NULL)
 		return (NULL);
