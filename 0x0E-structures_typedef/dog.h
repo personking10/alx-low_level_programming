5596840d84c9a2f357fb0e1c93cf6a36b96e2460
darsh <tefakong33@gmail.com>
2023-04-17 15:38:31 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _MOSTAFA_YOUSRY_
+#define _MOSTAFA_YOUSRY_
+/**
+ * struct dog - struct named dog
+ * @name: doggieee name
+ * @age: doggieee age
+ * @owner: doggiee name
+ * Description: doggiee struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
