95018ac7dd2c6d1efb2901ae7e66cff3f9fd6057
darsh <tefakong33@gmail.com>
2023-04-19 15:01:59 -0700

    a

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointer.h"
+/**
+ * print_name - printing a name
+ * @name: name of the named string
+ * @f: the function pointer has been printed
+ * Return: void
+ */
+void print_name(char *name, void(*f)(char*))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef MOSTAFA_YOUSRY_H
+#define MOSTAFA_YOUSRY_H
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
