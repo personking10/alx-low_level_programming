4ca205c3b2e2cb1eced618da406c0f074e65dd00
darsh <tefakong33@gmail.com>
2023-04-20 12:01:55 -0700

    a

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @separator: string
+ * @n: nums
+ * @...: ints
+ * Return: void
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+	int q = n;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (q--)
+		printf("%d%s", va_arg(ap, int),
+				q ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
